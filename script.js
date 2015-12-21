e6fae7bf6d69a3ec2ac72efe1266025ecfd63254
Erica Irving <contact.eirving@gmail.com>
2015-12-20 23:00:24 -0500

    update console log statements

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var now = new Date();
-console.log("Today is: " + now);
+// console.log("Today is: " + now);
 
 var nextWeek = new Date(now);
 nextWeek.setDate(nextWeek.getDate() + 7);
-console.log("One week from today is: " + nextWeek);
+// console.log("One week from today is: " + nextWeek);
 
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 var month = months[nextWeek.getMonth()];
